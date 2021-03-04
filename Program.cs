a7b1e26bd27abfb8a203852ea0b2765e9217d4e4
Reuel-T <19012817@vcconnect.co.za>
2021-03-04 10:50:49 +0200

    Hi

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Reuel!");
+        }
+    }
+}
